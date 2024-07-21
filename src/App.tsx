e4f196e6cb948aaa9d295a97d3a9071a27f0c8ab
jatin1213 <jatin.aggarwal.15csc@bml.edu.in>
2024-07-21 17:28:07 +0530

    Signup page added and to be continued

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={ <LoginPage /> } />
+				<Route path="/signup" element={<SignupPage />} />
 			</Routes>
 		</Router>
 	);
