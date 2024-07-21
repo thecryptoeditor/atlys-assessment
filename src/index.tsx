01cd462eb75a750698255e4b1db5bee6f7f19e2c
jatin1213 <jatin.aggarwal.15csc@bml.edu.in>
2024-07-21 17:09:43 +0530

    tailwind utilites added in base.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/base.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
